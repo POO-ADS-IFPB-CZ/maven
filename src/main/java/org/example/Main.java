d30d7a58a89fd6d4088bdf6f90ef20d7740a586c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-06-03 08:01:53 -0300

    testando os métodos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,12 @@ public class Main {
         Pessoa pessoa = new Pessoa("111.111.111-01",
                 "João", LocalDate.now());
         pessoa.setCpf("222.222.222-02");
+        System.out.println(pessoa.getCpf());
+
+        Pessoa pessoa1 = new Pessoa("222.222.222-02",
+                "João", LocalDate.now());
+        System.out.println(pessoa.equals(pessoa1));
+        System.out.println(pessoa);
 
     }
 }
