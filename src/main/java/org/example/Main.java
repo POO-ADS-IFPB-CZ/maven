788e4cadad95f4dd4f07e68c380d92c85826840c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-06-03 08:30:14 -0300

    Exemplo de uso do Gson

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,33 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.example.model.Pessoa;
+
+import java.lang.reflect.Type;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     static void main() {
-        Pessoa pessoa = new Pessoa("111.111.111-01",
-                "João", LocalDate.now());
-        pessoa.setCpf("222.222.222-02");
-        System.out.println(pessoa.getCpf());
 
-        Pessoa pessoa1 = new Pessoa("222.222.222-02",
-                "João", LocalDate.now());
-        System.out.println(pessoa.equals(pessoa1));
-        System.out.println(pessoa);
+        Gson gson = new Gson();
+        Set<Pessoa> pessoas = new HashSet<>();
+
+        pessoas.add(new Pessoa("111.111.111-01","João",
+                LocalDate.now()));
+        pessoas.add(new Pessoa("222.222.222-02", "Maria",
+                LocalDate.now()));
+        pessoas.add(new Pessoa("333.333.333-03", "Pedro",
+                LocalDate.now()));
+
+        String json = gson.toJson(pessoas);
+        System.out.println(json);
+
+        Type tipoColecao = new TypeToken<HashSet<Pessoa>>(){}.getType();
+        Set<Pessoa> pessoas2 = gson.fromJson(json, tipoColecao);
+        System.out.println(pessoas2);
 
     }
 }
